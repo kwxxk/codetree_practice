83a0fd5dd7d981241538f3f3ebe62279d9c44217
kwxxk <rkddnr55@gmail.com>
2026-09-17 15:46:36 +0900

    [Passed] $1 \times 3$ 안의 최대 동전 수 (45ms, 16MB)

--- trail2/$1 \times 3$ 안의 최대 동전 수/best-place-of-13.py
+++ trail2/$1 \times 3$ 안의 최대 동전 수/best-place-of-13.py
@@ -4,9 +4,6 @@ max_coin = 0
 # Please write your code here.
 for y in range(n):
     for x in range(n-2):
-        coin = 0
-        box = []
-        box = [grid[y][x]]+[grid[y][x+1]]+[grid[y][x+2]]
-        coin = sum(box)
+        coin = sum(grid[y][x:x+3])
         max_coin = max(max_coin,coin)
 print(max_coin)
